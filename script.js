1c5f5bbba034ac1b7b87a20c63ca3b1aa3311a43
ArturSad753 <artur207962@student-schoolsofwestfield.org>
2024-12-18 09:55:18 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function setup(){
+    let buttons = document.getElementById("buttons");
+    let ok = document.createElement("button");
+    let cancel = document.createElement("button");
+    ok.innerHTML = "ok";
+    ok.addEventListener("click",getPassword);
+    buttons.appendChild(ok);
+    cancel.innerHTML = "cancel";
+    buttons.appendChild(cancel);
+}
+function getPassword(){
+    let dialogs = document.getElementById("dialogs");
+    let username = dialogs.querySelector(':nth-child(2)').value; 
+    let password = dialogs.querySelector(':nth-child(4)').value;
+    alert(username+password);
 
+}
